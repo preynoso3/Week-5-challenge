252a7d28c4155d1f592259f26af6530cceab1fab
Pedro Reynoso <113930313+preynoso3@users.noreply.github.com>
2022-09-21 17:05:17 +0000

    add and remove from sets

--- main.py
+++ main.py
@@ -56,21 +56,22 @@ print(set2)
 print(5 in set1)
 # Join the following sets into one, called my_set_3:
 
-{1, 2, "three", "four"}
+set0={1, 2, "three", "four"}
 
-{"three", 4, 5}
-
-
+set01={"three", 4, 5}
+set3= set0.union(set01)
+print(set3)
 # Remove a random item from the following set, using set methods.
 
 raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
-
+raffle.remove("Ross")
+print(raffle)
 
 # Add the name Gunther to the following set, using set methods:
 
 raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-
+raffle.add("Pedro")
+print(raffle)
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
